d3089dffb92ea7bdaade93ca67ef1c3d82cb3ba0
Roberto <robertoabad93@gmail.com>
2020-11-23 10:18:31 +0100

    0497.02 añadimos el metodo de temperatura

--- Aire.java
+++ Aire.java
@@ -6,5 +6,35 @@
 
 public class Aire {
     
+    private double temperatura;
+    
+    
+    /**
+     * Aqui creamos el aire acondicionado
+     */
+    public Aire(){
+        temperatura = 15.0;
+    }
+    
+    /**
+     * Este metodo nos permite saber la temperatura
+     */
+    public double getTemperatura(){
+        return temperatura;
+    }
+    
+    /**
+     * Este metodo nos permite subir la temperatura
+     */
+    public void subirTemperatura(){
+        temperatura = temperatura + 5.0;
+    }
+    
+    /**
+     * Este metodo nos permite bajar la temperatura
+     */
+    public void bajarTemperatura(){
+        temperatura = temperatura - 5.0;
+    }
     
 }   
